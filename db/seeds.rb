8e97fe8751fde85f710c92c196a0a35919211a79
Patrick Bartels <patrick.bartels@gmail.com>
2022-01-26 11:32:25 +0100

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  seller = User.create! email: 'sally.seller@example.com', password: 'sally-seller-123', role: 'seller', username: 'sally-seller'
+
+  Product.create! name: 'Product 1', amount_available: 10, cost: 5, user: seller
+  Product.create! name: 'Product 2', amount_available: 10, cost: 10, user: seller
+  Product.create! name: 'Product 3', amount_available: 5, cost: 20, user: seller
+  Product.create! name: 'Product 4', amount_available: 5, cost: 15, user: seller
+  Product.create! name: 'Product 5', amount_available: 5, cost: 10, user: seller
+
+  User.create! email: 'burt.buyer@example.com', password: 'burt-buyer-123', role: 'buyer', deposit: 100, username: 'burt-buyer'
+end
