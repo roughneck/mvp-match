2d007a101f405de19f83642f4a2fc7e7d80f2ee6
Patrick Bartels <patrick.bartels@gmail.com>
2022-01-26 15:29:05 +0100

    Add devise controller overwrites

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,21 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    resource.persisted? ? register_success : register_failed
+  end
+
+  def register_success
+    render json: { message: 'Sign up successful.' }
+  end
+
+  def register_failed
+    render json: { message: "Sign up failed.", errors: resource.errors.full_messages }
+  end
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :username, :role)
+  end
+end
