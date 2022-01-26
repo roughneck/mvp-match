0525b8f0c710db8094da49610bfc2589bc3e3dd3
Patrick Bartels <patrick.bartels@gmail.com>
2022-01-26 11:51:08 +0100

    Add more validations to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :user
 
-  validates :amount_available, :cost, :name, :user, presence: true
+  validates :amount_available, :name, :user, presence: true
+  validates :cost, presence: true, numericality: { only_integer: true }
+
+  validate :cost_is_multiple_of_5
+
+  def cost_is_multiple_of_5
+    unless (self.cost % 5) == 0
+      errors.add(:cost, 'not multiple of 5')
+    end
+  end
 end
