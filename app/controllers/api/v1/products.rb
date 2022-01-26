cd2dc65147760d2c4cb6b7a5c1035210f41a8d94
Patrick Bartels <patrick.bartels@gmail.com>
2022-01-26 11:51:00 +0100

    Add product endpoints

--- app/controllers/api/v1/products.rb
+++ app/controllers/api/v1/products.rb
@@ -5,9 +5,49 @@ module API
         format :json
         prefix :api
         resource :products do
-          desc 'Return all Books'
+
+          desc 'Return all products'
           get do
             present Product.all
+          end
+
+          desc 'Return a specific product'
+          route_param :id do
+            get do
+              present Product.find(params[:id])
+            end
+          end
+
+          desc 'Create a new product'
+          params do
+            requires :name, type: String
+            requires :cost, type: Integer
+            requires :amount_available, type: Integer
+            requires :user_id, type: Integer
+          end
+          post do
+            present Product.create!(
+              name: params[:name],
+              cost: params[:cost],
+              amount_available: params[:amount_available],
+              user_id: params[:user_id]
+            )
+          end
+
+          desc 'Update a specific product'
+          route_param :id do
+            put do
+              product = Product.find(params[:id])
+              product.update(params)
+            end
+          end
+
+          desc 'Delete a specific product'
+          route_param :id do
+            delete do
+              product = Product.find(params[:id])
+              product.destroy
+            end
           end
         end
     end
