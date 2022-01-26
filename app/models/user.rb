200794a43126cc8e1baedb0c4be26bcd78234f9c
Patrick Bartels <patrick.bartels@gmail.com>
2022-01-26 15:28:42 +0100

    Set up jwt authentication for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
   validates :username, presence: true, uniqueness: true
   validates :role, presence: true, inclusion: { in: %w(buyer seller) }
